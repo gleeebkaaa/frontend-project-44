014b1c4b43860a25e4ac6c486900a04ab584b98c
gleeebkaaa <g.pudovkin1996@gmail.com>
2023-09-26 17:04:49 +0300

    rebrendind brain calc

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,37 +1,19 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import gameLogic from '../../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-
-console.log('What is the result of the expression?');
-
-let correctAnswerCount = 0;
-
-for (let i = 0; i < 3; i += 1) {
+const generateNumbers = () => {
     const num1 = Math.floor(Math.random() * 50);
     const num2 = Math.floor(Math.random() * 50);
     const operators = ['+', '-', '*'];
     const operator = operators[Math.floor(Math.random() * operators.length)];
-    const expression = `${num1} ${operator} ${num2}`;
-    console.log(`Questions: ${expression}`);
+    const question = `${num1} ${operator} ${num2}`;
     
-    const correctAnswer = eval(expression);
+    const correctAnswer = eval(question).toString();
 
-    const answer = Number(readlineSync.question('Your answer: '));
-    if (answer === correctAnswer) {
-        console.log('Correct!')
-        correctAnswerCount += 1;
-        if (correctAnswerCount === 3) {
-            console.log(`Congratulations, ${name}!`);
-            break;
-        }
-
-    } else {
-        console.log(`'${answer}' is wrong answer!; Correct answer was '${correctAnswer}'`);
-        console.log(`Let's try again, ${name}!`);
-        break;
-    }
+    return {question, correctAnswer};
 };
+
+const introMessage = 'What is the result of the expression?'
+gameLogic(generateNumbers, introMessage);
+
+    
