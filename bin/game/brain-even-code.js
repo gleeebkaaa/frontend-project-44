b928e906c2b04a7174b7d86914ec761489b8f09d
gleeebkaaa <g.pudovkin1996@gmail.com>
2023-10-07 12:26:50 +0300

    fix import brain-even

--- bin/game/brain-even-code.js
+++ bin/game/brain-even-code.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import gameLogic from '../../src/index.js';
-import { getRandomInt } from './helper.js';
+import getRandomInt from './helper.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
